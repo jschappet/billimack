4458b5575f04f42732f719a5aae50c010894ade5
Schappet, James C <james-schappet@uiowa.edu>
2017-10-09 08:11:31 -0500

    create cache data file

--- loadFiles.R
+++ loadFiles.R
@@ -1,23 +1,34 @@
 library(readr)
 
 
-filename_map <- read_csv("~/rSource/billimack/filename.map.csv", 
-   col_names = c("file_id","file_name","Barcode")) 
-
-
 readRnaFile <- function(input) {
   #print(input)
   data <- read_tsv(paste("./rna/",input$file_id,"/",input$file_name, sep=""), col_names = c("gene_id",input$Barcode) )
   data
 }
 
-nrow(filename_map)
 
-df <- readRnaFile(filename_map[1,])
+destfile="rna_all.rds" 
+if (!file.exists(destfile)) {
+  
+  filename_map <- read_csv("~/rSource/billimack/filename.map.csv", 
+                           col_names = c("file_id","file_name","Barcode")) 
+  
+  
 
-for (i in 2:nrow(filename_map)) {
-  newData <- readRnaFile(filename_map[i,])
-  df <- merge(df, newData,by="gene_id")
+  df <- readRnaFile(filename_map[1,])
+  
+  for (i in 2:nrow(filename_map)) {
+    newData <- readRnaFile(filename_map[i,])
+    df <- merge(df, newData,by="gene_id")
+    saveRDS(df, file=destfile)
+    
+  }
+  
+} else {
+  df <- readRDS(destfile)
   
 }
+
+
 
